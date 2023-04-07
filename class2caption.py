c87f55cfd030024be44d6b210d4a627db19a257b
uowol <alllhvhla@gmail.com>
2023-04-07 10:58:05 +0000

    update function

--- class2caption.py
+++ class2caption.py
@@ -283,6 +283,7 @@ if __name__ == '__main__':
     os.makedirs(dataset_path, exist_ok=True)
     domain_list = os.listdir(dataset_path)
     for domain_ in domain_list:
+        if domain_ != "sketch" : continue
         classes_path = os.path.join(dataset_path, domain_)
         os.makedirs(classes_path, exist_ok=True)
         classes_list = os.listdir(classes_path)
@@ -300,10 +301,7 @@ if __name__ == '__main__':
                 pil_image = PIL.Image.fromarray(image)
                 image = preprocess(pil_image).unsqueeze(0).to(device)
                 for class__ in classes_list:
-                    if class__ == class_:
-                        prompt = f"It is a {class_}."
-                    else:
-                        prompt = f"It is a {class__}. not a {class_}."
+                    prompt = f"It is a {class__}."
 
                     with torch.no_grad():
                         prefix = clip_model.encode_image(image).to(device, dtype=torch.float32)
@@ -319,11 +317,11 @@ if __name__ == '__main__':
                     generated_text_prefix = generated_text_prefix.strip()
                     if class_ in generated_text_prefix: 
                         generated_text_prefix = generated_text_prefix.replace(class_, class__)
-                    if class__ not in generated_text_prefix and class_ not in generated_text_prefix:
-                        if generated_text_prefix[-1] != '.' : generated_text_prefix = generated_text_prefix+'.'
-                        generated_text_prefix = generated_text_prefix+f' a photo of {class__}.'
+                    # if class__ not in generated_text_prefix and class_ not in generated_text_prefix:
+                    if generated_text_prefix[-1] != '.' : generated_text_prefix = generated_text_prefix+'.'
+                    generated_text_prefix = prompt +' '+ generated_text_prefix
 
-                    print(domain_, class_, name_, generated_text_prefix)
+                    print(f"{domain_}> {class_}> {name_}> {generated_text_prefix}")
                     with open(TO_SAVE_FILE, 'a') as f:
                         f.write(generated_text_prefix+'\n')
                 
